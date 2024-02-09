e909e8365c7bbaaf2bc4cab06c49503864bfb9f7
Replit user <>
2024-02-09 17:54:06 +0000

    Initial Commit

--- index.js
+++ index.js
@@ -18,12 +18,37 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
+const isInvalidDate = (date) => date.toUTCString() === "Invalid Date";
 
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+// your first API endpoint...
+app.get("/api/:date", function (req, res) {
+  let dateString = req.params.date;
+
+  // Try to convert the date string to a Date object
+  let date = new Date(dateString);
+
+  if(isInvalidDate(date)) {
+    date = new Date(+req.params.date);
+  }
+  // Check if the date is invalid
+  if (isInvalidDate(date)) {
+    res.json({ error: "Invalid Date" });
+    return;
+  }
+
+  res.json({
+    unix: date.getTime(),
+    utc: date.toUTCString()
+  });
 });
 
+
+app.get("/api", (req, res) => {
+  res.json({
+    unix: new Date().getTime(),
+    utc: new Date().toUTCString()
+  })
+})
 
 
 // listen for requests :)
